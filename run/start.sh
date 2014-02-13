68ff4bf0b279def73525eeb72e83c21f8081af2f
clay richardson <c.richardson917@gmail.com>
2014-02-13 09:38:48 +0000

    adding all daemons back into start script

--- run/start.sh
+++ run/start.sh
@@ -2,8 +2,7 @@
 set -xe
 /var/www/phabricator/bin/storage upgrade --force --trace
 /usr/bin/supervisord
-nice -n 20 sudo -u www-data /var/www/phabricator/bin/phd launch RepositoryPullLocal
-nice -n 20 sudo -u www-data /var/www/phabricator/bin/phd launch PhabricatorGarbageCollectorDaemon
+nice -n 20 sudo -u www-data /var/www/phabricator/bin/phd start
 # adapted from: http://stackoverflow.com/a/20932423/1050649
 while ( true )
   do
